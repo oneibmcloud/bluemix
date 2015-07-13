eec6f2bcd5b0241a4f6bc3cd2a70c0971f64561a
Michael McKay <mckaymic@us.ibm.com>
2015-07-13 16:19:34 -0400

    Added versions to package dependencies

--- package.js
+++ package.js
@@ -7,13 +7,13 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('underscore', 'client');
-  api.use('templating', 'client');
-  api.use('random', 'client');
-  api.use('service-configuration', ['client', 'server']);
+  api.use('oauth2@1.0.0', ['client', 'server']);
+  api.use('oauth@1.0.0', ['client', 'server']);
+  api.use('http@1.0.0', ['server']);
+  api.use('underscore@1.0.0', 'client');
+  api.use('templating@1.0.0', 'client');
+  api.use('random@1.0.0', 'client');
+  api.use('service-configuration@1.0.0', ['client', 'server']);
 
   api.export('Bluemix');
 
