fe6e31b8ec00ab1270e7624886573406f708f4f2
saurabh agarwal <agarwasa@us.ibm.com>
2016-08-04 17:31:47 -0400

    change the version to 1.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ibmcloud:bluemix',
-  version: '1.0.3',
+  version: '1.0.4',
   summary: 'IBM Bluemix OAuth flow',
   git: 'https://github.com/oneibmcloud/bluemix.git',
   documentation: 'README.md'
